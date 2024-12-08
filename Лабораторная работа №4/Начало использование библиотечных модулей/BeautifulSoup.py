3b94e771d86b0267887b4f2679fe0aa6c137b10d
Kidallov <158837916+Kidallov@users.noreply.github.com>
2024-12-08 17:24:07 +0300

    Update BeautifulSoup.py

--- Лабораторная работа №4/Начало использование библиотечных модулей/BeautifulSoup.py
+++ Лабораторная работа №4/Начало использование библиотечных модулей/BeautifulSoup.py
@@ -1,16 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-urls = {
-    "Текущая температура": "https://wttr.in/?format=%t",
-    "Скорость ветра": "https://wttr.in/?format=%w",
-    "Состояние погоды": "https://wttr.in/?format=%c"
-}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
-for name, url in urls.items():
-    response = requests.get(url)
-    result = response.text.strip()
+response = requests.get("https://wttr.in/Санкт-Петербург", headers=headers)
 
-    if name == "Скорость ветра":
-        result = result[1:]
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    weather_block = soup.find('pre')
 
-    print(f"{name}: {result}")
+    if weather_block:
+        print(weather_block.text.strip())
+    else:
+        print("Не удалось найти блок с погодой.")
+else:
+    print(f"Ошибка: не удалось загрузить данные (код {response.status_code}).")
